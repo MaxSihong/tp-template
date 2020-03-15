14d45f683185484780e1bd94f8528f110be9cda5
MaxSihong <13428038130@163.com>
2020-03-15 18:55:29 +0800

    暂创方法，需要考虑怎么处理全局异常会更好

---
+++ application/lib/exception/BaseException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: 陈志洪
+ * Date: 2020/3/15
+ * Time: 14:27
+ */
+
+namespace app\lib\exception;
+
+
+use think\Exception;
+
+class BaseException extends Exception
+{
+}
---
+++ application/lib/exception/ExceptionHandler.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: 陈志洪
+ * Date: 2020/3/15
+ * Time: 14:26
+ */
+
+namespace app\lib\exception;
+
+
+use Exception;
+use think\exception\Handle;
+use think\facade\Log;
+
+class ExceptionHandler extends Handle
+{
+}
